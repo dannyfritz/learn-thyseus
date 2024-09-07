9f53aa1d95ae19591d337abc7695676ab9cf5b34
Danny Fritz <me@dannyfritz.com>
2024-09-07 11:32:31 -0500

    WIP rain system

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {
   With,
   EventWriter,
   EventReader,
+  applyEntityUpdates,
 } from "thyseus";
 import { Mouse } from "./engine/Mouse";
 import { Keyboard } from "./engine/Keyboard";
@@ -23,12 +24,6 @@ import {
 import { Time } from "./engine/Time";
 import { Render } from "./engine/Render";
 
-const kaboomSound = new URL(`../hit.wav`, import.meta.url).href;
-const sound = new Howl({
-  src: [kaboomSound],
-});
-sound.load();
-
 type Event = "NOOP" | "KABOOM";
 
 class GameEvent {
@@ -44,6 +39,7 @@ function cleanupEvents(gameEvents: EventWriter<GameEvent>) {
 
 class IsPlayer extends Tag {}
 class IsBall extends Tag {}
+class IsRain extends Tag {}
 
 function kaboom(mouse: Res<Mouse>, gameEvents: EventWriter<GameEvent>) {
   if (mouse.isPressed(0)) {
@@ -57,42 +53,81 @@ function updatePlayerSystem(
   player: Query<[Position], With<IsPlayer>>,
 ) {
   const dt = time.dts;
-  for (const [position] of player) {
+  for (const [pos] of player) {
     if (keyboard.isDown("KeyK")) {
-      position.y -= 80 * dt;
+      pos.y -= 80 * dt;
     }
     if (keyboard.isDown("KeyJ")) {
-      position.y += 80 * dt;
+      pos.y += 80 * dt;
     }
     if (keyboard.isDown("KeyH")) {
-      position.x -= 80 * dt;
+      pos.x -= 80 * dt;
     }
     if (keyboard.isDown("KeyL")) {
-      position.x += 80 * dt;
+      pos.x += 80 * dt;
     }
   }
 }
 
-function renderBallSystem(render: Res<Render>, query: Query<[Position], With<IsBall>>) {
-  for (const [pos] of query) {
+function renderBallSystem(
+  render: Res<Render>,
+  rainDrops: Query<[Position], With<IsBall>>,
+) {
+  for (const [pos] of rainDrops) {
     render.circle(pos.x, pos.y);
   }
 }
 
 function renderCursorSystem(render: Res<Render>, mouse: Res<Mouse>) {
-    render.circle(mouse.pos.x, mouse.pos.y);
+  render.circle(mouse.pos.x, mouse.pos.y);
 }
 
-function renderPlayerSystem(render: Res<Render>, player: Query<[Position], With<IsPlayer>>) {
+function renderPlayerSystem(
+  render: Res<Render>,
+  player: Query<[Position], With<IsPlayer>>,
+) {
   for (const [position] of player) {
     render.circle(position.x, position.y);
   }
 }
 
+function renderRainSystem(
+  render: Res<Render>,
+  drops: Query<[Position], With<IsRain>>,
+) {
+  for (const [pos] of drops) {
+    if (!pos) continue;
+    render.circle(pos.x, pos.y);
+  }
+}
+
+function updateRainSystem(
+  world: World,
+  time: Res<Time>,
+  drops: Query<[Entity, Position], With<IsRain>>,
+) {
+  if (drops.length < 10) {
+    world.spawn().addType(IsRain).add(new Position(Math.random() * 200, 0));
+  }
+  for (const [entity, pos] of drops) {
+    if (!pos) continue;
+    pos.y += 80 * time.dts;
+    if (pos.y > 100) {
+      entity.despawn();
+    }
+  }
+}
+
+const createSound = (url: URL) => new Howl({ src: [url.href] }).load();
+
+const SOUNDS = {
+  hit: createSound(new URL("../hit.wav", import.meta.url)),
+} as const satisfies Record<string, Howl>;
+
 function renderAudio(gameEvents: EventReader<GameEvent>) {
   for (const gameEvent of gameEvents) {
     if (gameEvent.event === "KABOOM") {
-      sound.play();
+      SOUNDS.hit.play();
     }
   }
 }
@@ -100,11 +135,14 @@ function renderAudio(gameEvents: EventReader<GameEvent>) {
 const world = await new World()
   .addPlugin(baseEnginePlugin)
   .addPlugin(Render.plugin(StartSchedule, StopSchedule, RenderSchedule))
+  .addSystems(MainSchedule, applyEntityUpdates)
   .addSystems(MainSchedule, kaboom)
   .addSystems(MainSchedule, renderPlayerSystem)
   .addSystems(MainSchedule, renderBallSystem)
+  .addSystems(MainSchedule, renderRainSystem)
   .addSystems(MainSchedule, renderCursorSystem)
   .addSystems(MainSchedule, renderAudio)
+  .addSystems(MainSchedule, updateRainSystem)
   .addSystems(MainSchedule, updateVelocitySystem)
   .addSystems(MainSchedule, updatePlayerSystem)
   .addSystems(AfterSchedule, cleanupEvents)
